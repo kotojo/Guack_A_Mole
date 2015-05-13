b52c4f647321898cd00afdd613b3a32bb03028ef
Colin <colinrileytaylor@gmail.com>
2015-05-13 13:13:08 -0400

    have board laid out am implementing avocados

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+  //materialzie menu will come bback to
+//   $('.button-collapse').sideNav({
+//     menuWidth: 300,
+//     edge: 'right',
+//     closeOnClick: true
+//   }
+// );
+
+});
