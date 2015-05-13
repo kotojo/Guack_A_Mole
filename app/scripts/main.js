bef5c864b713c8f9ac474b6367d6b12616a515e5
Colin <colinrileytaylor@gmail.com>
2015-05-13 15:45:39 -0400

    very basic functionality not a game just things dissapearing upon click

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,19 @@
+'use strict';
 $(function(){
-  //materialzie menu will come bback to
-//   $('.button-collapse').sideNav({
-//     menuWidth: 300,
-//     edge: 'right',
-//     closeOnClick: true
-//   }
+  // materialzie menu will come bback to
+  // $('.button-collapse').sideNav({
+  //   menuWidth: 300,
+  //   edge: 'right',
+  //   closeOnClick: true
+  // });
 // );
 
 });
+
+
+$('.avo').click(function(e){
+    e.preventDefault();
+    $(this).removeClass('avo').addClass('guacamole').delay(1000).fadeOut('explode');
+  });
+
+
