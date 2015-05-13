b1351658a009c141e3cb50879d04c2ed30985e1a
Colin <colinrileytaylor@gmail.com>
2015-05-13 19:07:08 -0400

    now have avos reaapearing but not consistently

--- app/scripts/second.js
+++ app/scripts/second.js
@@ -5,15 +5,24 @@ function disappear() {
 }
 
 function reappear() {
-  $('disappear').addClass('avo').removeClass('disappear');
+  $('.disappear').addClass('avo').removeClass('disappear');
 }
+
+function reappear2(){
+$('div').each(function(){
+  if($(this).hasClass('disappear')){
+    $(this).addClass('avo').removeClass('disappear');
+  }
+});
+}
+
 
 function clickToChange(){
   $('.avo').click(function(){
     $(this).removeClass('avo').addClass('guacamole').delay(1500).queue(function(){
       disappear();
     });
-
+setTimeout(reappear2, 3000);
   });
 }
 
