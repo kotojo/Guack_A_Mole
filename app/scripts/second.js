80a5f436d3920e3d14c8e8f4710ee7524ec51270
Colin <colinrileytaylor@gmail.com>
2015-05-14 11:57:15 -0400

    basic game loop will cycle indefinitely

--- app/scripts/second.js
+++ app/scripts/second.js
@@ -3,6 +3,7 @@ $(function(){
 
   function disappear() {
     $('.guacamole').removeClass('guacamole').addClass('disappear');
+    console.log('stuff');
   }
 
   // function reappear() {
@@ -18,19 +19,43 @@ $(function(){
   });
   }
 
-
-    $('.avo').click(function(){
-      $(this).removeClass('avo').addClass('guacamole').delay(1500).queue(function(){
-        disappear();
-        // setTimeout(reappear2, 500);
-        setTimeout(document.location.reload(true), 500);
+//click handler for turning avos into guac
+function handleClick(id){
+    $(id).click(function(){
+      $(this).removeClass('avo').addClass('guacamole');
+      setTimeout(function(){
+        $('.guacamole').removeClass('guacamole').addClass('disappear');
+    console.log('stuff');
+      }, 1000);
+      setTimeout(function(){
+         $('div').each(function(){
+    if($(this).hasClass('disappear')){
+      $(this).addClass('avo').removeClass('disappear');
+      console.log('reset');
+    }
+  });
+       }, 1000);
+        // setTimeout(disappear(), 1500);
+        // setTimeout(reappear2(), 500);
+        // setTimeout(document.location.reload(true), 500);
       });
 
-    });
+    }
 
+handleClick('#avo1');
+handleClick('#avo2');
+handleClick('#avo3');
+handleClick('#avo4');
+handleClick('#avo5');
+handleClick('#avo6');
+
+//Animate avos up and down
+// setTimeout($('#avo4').animateCSS("slideOutDown"), 1000);
 
 
 // clickToChange();
+
+
 
 });
 
