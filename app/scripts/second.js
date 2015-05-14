1e0408e87d885ecc138a378287d07d82955a149a
Colin <colinrileytaylor@gmail.com>
2015-05-14 09:40:54 -0400

    avos will now refresh indefinitely but must reload entire document

--- app/scripts/second.js
+++ app/scripts/second.js
@@ -1,5 +1,5 @@
 'use strict';
-
+$(function(){
 
   function disappear() {
     $('.guacamole').removeClass('guacamole').addClass('disappear');
@@ -18,18 +18,23 @@
   });
   }
 
-  function clickToChange() {
+
     $('.avo').click(function(){
       $(this).removeClass('avo').addClass('guacamole').delay(1500).queue(function(){
         disappear();
-        setTimeout(reappear2, 500);
+        // setTimeout(reappear2, 500);
+        setTimeout(document.location.reload(true), 500);
       });
 
     });
-  }
 
 
-clickToChange();
+
+// clickToChange();
+
+});
+
+
 
 
 
