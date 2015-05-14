84e737493617c0b6ee1ab76e629455f7d4eeadcf
Colin <colinrileytaylor@gmail.com>
2015-05-14 15:18:15 -0400

    random second generator added

--- app/scripts/second.js
+++ app/scripts/second.js
@@ -1,15 +1,12 @@
 'use strict';
 $(function(){
-
+var x = 0;
+//handles the dissapearnce
   function disappear() {
     $('.guacamole').removeClass('guacamole').addClass('disappear');
-    console.log('stuff');
+     console.log('stuff');
   }
-
-  // function reappear() {
-  //   $('.disappear').addClass('avo').removeClass('disappear');
-  // }
-
+//handles the reappearnace
   function reappear2(){
   $('div').each(function(){
     if($(this).hasClass('disappear')){
@@ -25,9 +22,7 @@ function handleClick(id){
     $(this).removeClass('avo').addClass('guacamole');
     setTimeout(disappear, 1000);
     setTimeout(reappear2, 3000);
-    // setTimeout(disappear(), 1500);
-    // setTimeout(reappear2(), 500);
-    // setTimeout(document.location.reload(true), 500);
+    x++;
   });
 }
 
@@ -41,9 +36,37 @@ handleClick('#avo6');
 //Animate avos up and down
 function animateUpDown(id){
   // $(id).an
+  // setTimeout($(id).animateCSS('slideInUp'), 3000);
+}
+//Random Second generator
+var seconds;
+function randomSeconds() {
+  seconds = Math.random() * 10000;
+  console.log(seconds);
 }
 
-// clickToChange();
+//Animate avos up and down
+function randomShow(){
+  randomSeconds();
+  $('#avo1').delay(seconds).fadeOut('fast').delay(seconds).fadeIn('fast');
+  randomSeconds();
+  $('#avo2').delay(seconds).fadeOut('fast').delay(seconds).fadeIn('fast');
+  randomSeconds();
+  $('#avo3').delay(seconds).fadeOut('fast').delay(seconds).fadeIn('fast');
+  randomSeconds();
+  $('#avo4').delay(seconds).fadeOut('fast').delay(seconds).fadeIn('fast');
+  randomSeconds();
+  $('#avo5').delay(seconds).fadeOut('fast').delay(seconds).fadeIn('fast');
+  randomSeconds();
+  $('#avo6').delay(seconds).fadeOut('fast').delay(seconds).fadeIn('fast');
+}
+
+
+// animateUpDown('#avo1');
+
+//loop to continuosly dissappear avos
+setInterval(randomShow, seconds);
+
 
 
 
