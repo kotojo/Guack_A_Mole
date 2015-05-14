9368fdfbaa539998e37a8c000225089fe25bfd44
Colin <colinrileytaylor@gmail.com>
2015-05-13 21:15:25 -0400

    not much updated here

--- app/scripts/second.js
+++ app/scripts/second.js
@@ -1,29 +1,36 @@
 'use strict';
 
-function disappear() {
-  $('.guacamole').removeClass('guacamole').addClass('disappear');
-}
 
-function reappear() {
-  $('.disappear').addClass('avo').removeClass('disappear');
-}
-
-function reappear2(){
-$('div').each(function(){
-  if($(this).hasClass('disappear')){
-    $(this).addClass('avo').removeClass('disappear');
+  function disappear() {
+    $('.guacamole').removeClass('guacamole').addClass('disappear');
   }
-});
-}
 
+  // function reappear() {
+  //   $('.disappear').addClass('avo').removeClass('disappear');
+  // }
 
-function clickToChange(){
-  $('.avo').click(function(){
-    $(this).removeClass('avo').addClass('guacamole').delay(1500).queue(function(){
-      disappear();
-    });
-setTimeout(reappear2, 3000);
+  function reappear2(){
+  $('div').each(function(){
+    if($(this).hasClass('disappear')){
+      $(this).addClass('avo').removeClass('disappear');
+      console.log('reset');
+    }
   });
-}
+  }
+
+  function clickToChange() {
+    $('.avo').click(function(){
+      $(this).removeClass('avo').addClass('guacamole').delay(1500).queue(function(){
+        disappear();
+        setTimeout(reappear2, 500);
+      });
+
+    });
+  }
+
+
+clickToChange();
+
+
 
 
