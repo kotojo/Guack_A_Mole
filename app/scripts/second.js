6c2058abdcfccf557ba725ca01d883c42608bd9d
Colin <colinrileytaylor@gmail.com>
2015-05-14 20:30:05 -0400

    have gotten timer and end screen working and sound sort of

--- app/scripts/second.js
+++ app/scripts/second.js
@@ -1,6 +1,7 @@
 'use strict';
 $(function(){
-var x = 0;
+var score = 0;
+var gameTime = 20;
 //handles the dissapearnce
   function disappear() {
     $('.guacamole').removeClass('guacamole').addClass('disappear');
@@ -22,7 +23,10 @@ function handleClick(id){
     $(this).removeClass('avo').addClass('guacamole');
     setTimeout(disappear, 1000);
     setTimeout(reappear2, 3000);
-    x++;
+    score++;
+    console.log(score);
+    printScore();
+    $('#sound1').get(0).play();
   });
 }
 
@@ -42,7 +46,7 @@ function animateUpDown(id){
 var seconds;
 function randomSeconds() {
   seconds = Math.random() * 10000;
-  console.log(seconds);
+  // console.log(seconds);
 }
 
 //Animate avos up and down
@@ -60,13 +64,56 @@ function randomShow(){
   randomSeconds();
   $('#avo6').delay(seconds).fadeOut('fast').delay(seconds).fadeIn('fast');
 }
+function printScore(){
+  $('#score').html('Score: ' + score);
+}
 
+function timer(){
+  gameTime--;
+  $('#timer').html('Time Left: ' + gameTime + ' sec');
+  if(gameTime === 0){
+    clearInterval(myTimer);
+    console.log(0);
+    $('#element_to_pop_up').bPopup({
+      speed: 450,
+      transition: 'slideIn',
+      transitionClose: 'slideBack', //can be a string ('slow'/'fast') or int
+      followSpeed: 1500, //can be a string ('slow'/'fast') or int
+      modalColor: 'greenYellow',
+      content: 'image',
+      contentContainer: '.content',
+      loadUrl: '../images/holy-guacamole.png'
+    });
+
+  };
+}
+//resets score and time and clears scoreboard
+function resetGame (){
+  gameTime = 0;
+  score = 0;
+  $('#score').html('Score: ' + score);
+}
+
+
+//Play Again button
+function playAgain(){
+var bPopup = $('#element_to_pop_up').bPopup();
+$('#replay').click(function(){
+  resetGame();
+  console.log('reset');
+  $('#element_to_pop_up').css('display', 'none');
+  $('.b-modal __b-popup1__').css('display', 'none');
+  bPopup.close();
+});
+}
+// var countdownTimer = setInterval(gameTimer(), 1000);
 
 // animateUpDown('#avo1');
 
 //loop to continuosly dissappear avos
-setInterval(randomShow, seconds);
+var showAvos = setInterval(randomShow, seconds);
 
+var myTimer = setInterval(timer, 1000);
 
 
 
