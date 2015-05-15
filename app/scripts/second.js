8dd41ff069121f11ae58ab8e740969ce2ade73a6
Colin <colinrileytaylor@gmail.com>
2015-05-15 13:16:13 -0400

    added a side nav and some sounds fixed most timing issues

--- app/scripts/second.js
+++ app/scripts/second.js
@@ -1,7 +1,12 @@
 'use strict';
 $(function(){
 var score = 0;
-var gameTime = 20;
+var gameTime = 10;
+var runTime = 20;
+var myTimer;
+var seconds;
+var showAvos;
+ $('.button-collapse').sideNav();
 //handles the dissapearnce
   function disappear() {
     $('.guacamole').removeClass('guacamole').addClass('disappear');
@@ -37,35 +42,31 @@ handleClick('#avo4');
 handleClick('#avo5');
 handleClick('#avo6');
 
-//Animate avos up and down
-function animateUpDown(id){
-  // $(id).an
-  // setTimeout($(id).animateCSS('slideInUp'), 3000);
-}
 //Random Second generator
-var seconds;
+
 function randomSeconds() {
-  seconds = Math.random() * 10000;
+  seconds = Math.random() * 4000;
   // console.log(seconds);
 }
 
 //Animate avos up and down
 function randomShow(){
   randomSeconds();
-  $('#avo1').delay(seconds).fadeOut('fast').delay(seconds).fadeIn('fast');
+  $('#avo1').delay(seconds).slideToggle('fast');
   randomSeconds();
-  $('#avo2').delay(seconds).fadeOut('fast').delay(seconds).fadeIn('fast');
+  $('#avo2').delay(seconds).slideToggle('fast');
   randomSeconds();
-  $('#avo3').delay(seconds).fadeOut('fast').delay(seconds).fadeIn('fast');
+  $('#avo3').delay(seconds).slideToggle('fast');
   randomSeconds();
-  $('#avo4').delay(seconds).fadeOut('fast').delay(seconds).fadeIn('fast');
+  $('#avo4').delay(seconds).slideToggle('fast');
   randomSeconds();
-  $('#avo5').delay(seconds).fadeOut('fast').delay(seconds).fadeIn('fast');
+  $('#avo5').delay(seconds).slideToggle('fast');
   randomSeconds();
-  $('#avo6').delay(seconds).fadeOut('fast').delay(seconds).fadeIn('fast');
+  $('#avo6').delay(seconds).slideToggle('fast');
 }
 function printScore(){
   $('#score').html('Score: ' + score);
+  console.log(score);
 }
 
 function timer(){
@@ -73,47 +74,57 @@ function timer(){
   $('#timer').html('Time Left: ' + gameTime + ' sec');
   if(gameTime === 0){
     clearInterval(myTimer);
-    console.log(0);
-    $('#element_to_pop_up').bPopup({
+    console.log(gameTime);
+    popupEndGame();
+  };
+}
+//resets score and time and clears scoreboard
+function resetGame (){
+  gameTime = 8;
+  score = 0;
+  $('#timer').html('Time Left: ' + gameTime + ' sec');
+  $('#score').html('Score: ' + score);
+  myTimer = setInterval(timer, 1000);
+}
+
+
+//Play Again button
+$('#replay').click(function(){
+    var bPopup = $('#element_to_pop_up').bPopup();
+    resetGame();
+    console.log('playagain button');
+    $('#element_to_pop_up').css('display', 'none');
+    $('.b-modal __b-popup1__').css('display', 'none');
+    bPopup.close();
+});
+
+//Menu/Popup elements
+function popupEndGame(){
+$('#element_to_pop_up').bPopup({
       speed: 450,
       transition: 'slideIn',
       transitionClose: 'slideBack', //can be a string ('slow'/'fast') or int
       followSpeed: 1500, //can be a string ('slow'/'fast') or int
       modalColor: 'greenYellow',
-      content: 'image',
-      contentContainer: '.content',
-      loadUrl: '../images/holy-guacamole.png'
     });
-
-  };
 }
-//resets score and time and clears scoreboard
-function resetGame (){
-  gameTime = 0;
-  score = 0;
-  $('#score').html('Score: ' + score);
-}
-
-
-//Play Again button
-function playAgain(){
-var bPopup = $('#element_to_pop_up').bPopup();
-$('#replay').click(function(){
-  resetGame();
-  console.log('reset');
-  $('#element_to_pop_up').css('display', 'none');
-  $('.b-modal __b-popup1__').css('display', 'none');
-  bPopup.close();
+$('#startbutton').click(function(){
+  startGame();
+  console.log('start clicked');
+  $('#startSound').get(0).play();
 });
-}
-// var countdownTimer = setInterval(gameTimer(), 1000);
-
-// animateUpDown('#avo1');
 
 //loop to continuosly dissappear avos
-var showAvos = setInterval(randomShow, seconds);
 
-var myTimer = setInterval(timer, 1000);
+function startShow(){
+    showAvos = setInterval(randomShow, seconds);
+}
+function startGame(){
+  startShow();
+  myTimer = setInterval(timer, 1000);
+}
+
+
 
 
 
